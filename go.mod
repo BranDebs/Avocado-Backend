2c97a3cd2a435c6f4f17d253961c58d9ae644bb0
Brandon Yeo <brandonyeoxg@gmail.com>
2020-05-05 21:31:33 +0800

    feat(repository): add postgres repo (#9)

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/go-chi/chi v4.1.0+incompatible
+	github.com/jinzhu/gorm v1.9.12
 	github.com/pkg/errors v0.9.1
 )
---
+++ repository/postgres/repository.go
@@ -0,0 +1,84 @@
+package postgres
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/BranDebs/Avocado-Backend/account"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var (
+	ErrDupAccount error = errors.New("duplicate account error")
+)
+
+type postgresRepository struct {
+	db *gorm.DB
+}
+
+type ConnSettings struct {
+	Host     string
+	Port     int64
+	User     string
+	DBName   string
+	Password string
+}
+
+func (c ConnSettings) String() string {
+	return fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s",
+		c.Host, c.Port, c.User, c.DBName, c.Password)
+}
+
+func newGormDB(connStr string) (*gorm.DB, error) {
+	db, err := gorm.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func NewPostgresRepository(settings ConnSettings) (account.AccountRepository, error) {
+	db, err := newGormDB(settings.String())
+	if err != nil {
+		return nil, err
+	}
+	repo := postgresRepository{
+		db: db,
+	}
+	return &repo, nil
+}
+
+func (repo *postgresRepository) Find(email string) (*account.Account, error) {
+	var acc *account.Account
+	res := repo.db.Where(account.Account{Email: email}).First(acc)
+	if res.Error != nil {
+		return nil, res.Error
+	}
+	return acc, nil
+}
+
+func (repo *postgresRepository) Store(account *account.Account) error {
+	if repo.db.NewRecord(account) {
+		// can store
+		repo.db.Create(account)
+		return nil
+	}
+	return ErrDupAccount
+}
+
+func (repo *postgresRepository) Delete(email string) (*account.Account, error) {
+	var acc *account.Account
+	acc, err := repo.Find(email)
+	if err != nil {
+		return nil, err
+	}
+
+	res := repo.db.Delete(acc)
+	if res.Error != nil {
+		return nil, res.Error
+	}
+
+	return acc, nil
+}
