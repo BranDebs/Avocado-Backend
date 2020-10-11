b81961092956e087d18887b2fd0e926004d9f4a4
brandonyeoxg <brandonyeoxg@gmail.com>
2020-10-11 11:26:07 +0800

    feat(account): update to login, register and delete

--- account/logic.go
+++ account/logic.go
@@ -1,5 +1,26 @@
 package account
 
+import (
+	"bytes"
+	"crypto/rand"
+	"errors"
+	"fmt"
+	"io"
+
+	"golang.org/x/crypto/scrypt"
+	"gorm.io/gorm"
+)
+
+const (
+	PW_SALT_BYTES = 32
+	PW_HASH_BYTES = 64
+)
+
+var (
+	ErrRecordNotFound = gorm.ErrRecordNotFound
+	ErrDuplicateEmail = errors.New("email already exists")
+)
+
 type accountService struct {
 	accountRepo AccountRepository
 }
@@ -15,9 +36,32 @@ func (s *accountService) Find(email string) (*Account, error) {
 }
 
 func (s *accountService) Store(account *Account) error {
+	salt := make([]byte, PW_SALT_BYTES)
+	_, err := io.ReadFull(rand.Reader, salt)
+	if err != nil {
+		return fmt.Errorf("AccountService.Store: generating salt: %w", err)
+	}
+
+	hash, err := scrypt.Key([]byte(account.Password), salt, 1<<14, 8, 1, PW_HASH_BYTES)
+	if err != nil {
+		return fmt.Errorf("AccountService.Store: generating password hash: %w", err)
+	}
+
+	account.Password = hash
+	account.PasswordSalt = salt
+
 	return s.accountRepo.Store(account)
 }
 
 func (s *accountService) Delete(email string) (*Account, error) {
 	return s.accountRepo.Delete(email)
+}
+
+func (s *accountService) Verify(acc *Account, password string) (bool, error) {
+	hash, err := scrypt.Key([]byte(password), acc.PasswordSalt, 1<<14, 8, 1, PW_HASH_BYTES)
+	if err != nil {
+		return false, fmt.Errorf("AccountService.Verify: generating password hash: %w", err)
+	}
+
+	return bytes.Equal(hash, acc.Password), nil
 }
--- api/http.go
+++ api/http.go
@@ -1,27 +1,31 @@
 package api
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io"
 	"log"
 	"net/http"
 
 	"github.com/BranDebs/Avocado-Backend/account"
+	"github.com/go-chi/chi"
 )
 
 const (
 	ContentTypeJSON = "application/json"
+
+	CreateAccountErrMsg = "Failed to create an account."
+	LoginAccountErrMsg  = "Failed to login into account."
+	DeleteAccountErrMsg = "Failed to delete account."
+
+	DefaultErrDetail  = "Something wrong happened on our end, try again in 30 minutes time."
+	JSONBodyErrDetail = "Ensure that request body is a valid JSON object."
 )
 
-func setupResponse(w http.ResponseWriter, contentType string, statusCode int, body []byte) {
-	w.Header().Set("Content-Type", contentType)
-	w.WriteHeader(statusCode)
-	if _, err := w.Write(body); err != nil {
-		log.Println(err)
-	}
-}
-
 type Handler interface {
-	PostAccount(http.ResponseWriter, *http.Request)
-	GetAccount(http.ResponseWriter, *http.Request)
+	CreateAccount(http.ResponseWriter, *http.Request)
+	LoginAccount(http.ResponseWriter, *http.Request)
 	DeleteAccount(http.ResponseWriter, *http.Request)
 
 	Ping(http.ResponseWriter, *http.Request)
@@ -41,14 +45,164 @@ func (*handler) Ping(w http.ResponseWriter, r *http.Request) {
 	setupResponse(w, ContentTypeJSON, http.StatusOK, []byte(`{"message": "Pong!"}`))
 }
 
-func (h *handler) PostAccount(w http.ResponseWriter, r *http.Request) {
-	setupResponse(w, ContentTypeJSON, http.StatusOK, []byte(`{"message":"Post Account"}`))
+func (h *handler) CreateAccount(w http.ResponseWriter, r *http.Request) {
+	var bbuf bytes.Buffer
+
+	if _, err := io.Copy(&bbuf, r.Body); err != nil {
+		setupError(w, ContentTypeJSON, http.StatusUnprocessableEntity, &Error{
+			Message: CreateAccountErrMsg,
+			Detail:  JSONBodyErrDetail,
+			Cause:   err,
+		})
+		return
+	}
+	defer r.Body.Close()
+
+	var acc Account
+	if err := json.Unmarshal(bbuf.Bytes(), &acc); err != nil {
+		setupError(w, ContentTypeJSON, http.StatusInternalServerError, &Error{
+			Message: CreateAccountErrMsg,
+			Detail:  DefaultErrDetail,
+			Cause:   err,
+		})
+		return
+	}
+
+	if err := h.acctSvc.Store(&account.Account{Email: acc.Email, Password: []byte(acc.Password)}); err != nil {
+		// log.Printf("Handler.CreateAccount could not store on the database: %s", err)
+		if errors.Is(err, account.ErrDuplicateEmail) {
+			setupError(w, ContentTypeJSON, http.StatusUnprocessableEntity, &Error{
+				Message: CreateAccountErrMsg,
+				Detail:  "You already have an account with us.",
+				Cause:   err,
+			})
+			return
+		}
+
+		setupError(w, ContentTypeJSON, http.StatusInternalServerError, &Error{
+			Message: CreateAccountErrMsg,
+			Detail:  DefaultErrDetail,
+			Cause:   err,
+		})
+		return
+	}
+
+	acc.Password = ""
+
+	resp, err := json.Marshal(&acc)
+	if err != nil {
+		setupError(w, ContentTypeJSON, http.StatusInternalServerError, &Error{
+			Message: CreateAccountErrMsg,
+			Detail:  DefaultErrDetail,
+			Cause:   err,
+		})
+		return
+	}
+
+	setupResponse(w, ContentTypeJSON, http.StatusOK, resp)
 }
 
-func (h *handler) GetAccount(w http.ResponseWriter, r *http.Request) {
-	setupResponse(w, ContentTypeJSON, http.StatusOK, []byte(`{"message":"Get Account"}`))
+func (h *handler) LoginAccount(w http.ResponseWriter, r *http.Request) {
+	var bbuf bytes.Buffer
+
+	if _, err := io.Copy(&bbuf, r.Body); err != nil {
+		setupError(w, ContentTypeJSON, http.StatusUnprocessableEntity, &Error{
+			Message: LoginAccountErrMsg,
+			Detail:  JSONBodyErrDetail,
+		})
+		return
+	}
+	defer r.Body.Close()
+
+	var acc Account
+	if err := json.Unmarshal(bbuf.Bytes(), &acc); err != nil {
+		setupError(w, ContentTypeJSON, http.StatusUnprocessableEntity, &Error{
+			Message: LoginAccountErrMsg,
+			Detail:  JSONBodyErrDetail,
+			Cause:   err,
+		})
+		return
+	}
+
+	foundAcc, err := h.acctSvc.Find(acc.Email)
+	if err != nil {
+		errCode := http.StatusInternalServerError
+		detail := DefaultErrDetail
+		if errors.Is(err, account.ErrRecordNotFound) {
+			errCode = http.StatusNotFound
+			detail = "Email does not exists."
+		}
+		setupError(w, ContentTypeJSON, errCode, &Error{
+			Message: LoginAccountErrMsg,
+			Detail:  detail,
+			Cause:   err,
+		})
+		return
+	}
+
+	success, err := h.acctSvc.Verify(foundAcc, acc.Password)
+	if err != nil {
+		setupError(w, ContentTypeJSON, http.StatusInternalServerError, &Error{
+			Message: LoginAccountErrMsg,
+			Detail:  DefaultErrDetail,
+			Cause:   err,
+		})
+		return
+	}
+
+	if !success {
+		setupError(w, ContentTypeJSON, http.StatusUnauthorized, &Error{
+			Message: LoginAccountErrMsg,
+			Detail:  "Invalid login credentials.",
+			Cause:   err,
+		})
+		return
+	}
+
+	resp, err := json.Marshal(&acc)
+	if err != nil {
+		setupError(w, ContentTypeJSON, http.StatusInternalServerError, &Error{
+			Message: LoginAccountErrMsg,
+			Detail:  DefaultErrDetail,
+			Cause:   err,
+		})
+		return
+	}
+
+	setupResponse(w, ContentTypeJSON, http.StatusOK, resp)
 }
 
 func (h *handler) DeleteAccount(w http.ResponseWriter, r *http.Request) {
-	setupResponse(w, ContentTypeJSON, http.StatusOK, []byte(`{"message":"Delete Account"}`))
+	email := chi.URLParam(r, "email")
+
+	if _, err := h.acctSvc.Delete(email); err != nil {
+		errCode := http.StatusInternalServerError
+		detail := DefaultErrDetail
+		if errors.Is(err, account.ErrRecordNotFound) {
+			errCode = http.StatusNotFound
+			detail = "Email does not exists."
+		}
+		setupError(w, ContentTypeJSON, errCode, &Error{
+			Message: DeleteAccountErrMsg,
+			Detail:  detail,
+			Cause:   err,
+		})
+		return
+	}
+
+	setupResponse(w, ContentTypeJSON, http.StatusNoContent, nil)
+}
+
+func setupResponse(w http.ResponseWriter, contentType string, statusCode int, body []byte) {
+	w.Header().Set("Content-Type", contentType)
+	w.WriteHeader(statusCode)
+	if _, err := w.Write(body); err != nil {
+		log.Println(err)
+	}
+}
+
+func setupError(w http.ResponseWriter, contentType string, statusCode int, err *Error) {
+	resp, _ := json.Marshal(err)
+	log.Printf("%s: %s", err.Message, err.Cause)
+	setupResponse(w, contentType, statusCode, resp)
 }
