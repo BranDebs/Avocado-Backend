2156e2466820e7b828d3f3a1ea5f8d388fa4581f
brandonyeoxg <brandonyeoxg@gmail.com>
2021-04-04 11:27:05 +0800

    add nit

--- account/service.go
+++ account/service.go
@@ -1,7 +1,7 @@
 package account
 
 type AccountService interface {
-	Store(*Account) error
+	Store(acc *Account) error
 	Find(email string) (*Account, error)
 	Delete(email string) (*Account, error)
 	Verify(acc *Account, password string) (string, error)
