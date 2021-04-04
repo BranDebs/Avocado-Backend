69a3d44185ed93f2ff40de692e62689eb1605f98
brandonyeoxg <brandonyeoxg@gmail.com>
2021-04-04 12:22:52 +0800

    feat(task): add account specific repository

--- repository/postgres/repository.go
+++ repository/postgres/repository.go
@@ -1,19 +1,15 @@
 package postgres
 
 import (
-	"errors"
 	"fmt"
 
-	"github.com/BranDebs/Avocado-Backend/account"
 	"github.com/BranDebs/Avocado-Backend/secrets"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-type postgresRepository struct {
-	db *gorm.DB
-}
+var db *gorm.DB
 
 type ConnSettings struct {
 	Host         string `mapstructure:"host"`
@@ -31,62 +27,14 @@ func (c ConnSettings) String() string {
 }
 
 func newGormDB(connStr string) (*gorm.DB, error) {
+	if db != nil {
+		return db, nil
+	}
+
 	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
+
 	return db, nil
-}
-
-func NewRepository(settings ConnSettings) (account.AccountRepository, error) {
-	db, err := newGormDB(settings.String())
-	if err != nil {
-		return nil, err
-	}
-
-	if err := db.AutoMigrate(&account.Account{}); err != nil {
-		return nil, err
-	}
-
-	repo := postgresRepository{
-		db: db,
-	}
-	return &repo, nil
-}
-
-func (repo *postgresRepository) Find(email string) (*account.Account, error) {
-	var acc account.Account
-	res := repo.db.Where(account.Account{Email: email}).First(&acc)
-	if res.Error != nil {
-		if errors.Is(res.Error, gorm.ErrRecordNotFound) {
-			return nil, account.ErrRecordNotFound
-		}
-		return nil, res.Error
-	}
-	return &acc, nil
-}
-
-func (repo *postgresRepository) Store(acc *account.Account) error {
-	if _, err := repo.Find(acc.Email); err == nil {
-		return account.ErrDuplicateEmail
-	}
-
-	if err := repo.db.Create(acc).Error; err != nil {
-		return err
-	}
-	return nil
-}
-
-func (repo *postgresRepository) Delete(email string) (*account.Account, error) {
-	var acc *account.Account
-	acc, err := repo.Find(email)
-	if err != nil {
-		return nil, err
-	}
-
-	res := repo.db.Delete(acc)
-	if res.Error != nil {
-		return nil, res.Error
-	}
-	return acc, nil
 }
